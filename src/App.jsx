323eb400381355c817d4ce9b9fea2818220b6a69
sahithi0504 <srampa1@lsu.edu>
2025-03-21 23:38:27 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to Financial Hackathon 💰</h1>
+      <p>Built with React + Vite</p>
+    </div>
+  );
+}
+
+export default App;
